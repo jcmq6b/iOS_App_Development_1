51d6e91df0635512118979278065d2a571be9efc
jcmq6b <jcmq6b@mail.missouri.edu>
2020-07-29 18:13:12 -0500

    made changes
    added color struct and set background on cells

---
+++ ColorsWithNavigation/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Jessica Murphey on 7/29/20.
+//  Copyright © 2020 Jessica Murphey. All rights reserved.
+//
+
+import UIKit
+//Has default init, can create without explicitly defining
+struct Color{
+    let name: String
+    let uiColor: UIColor
+}
